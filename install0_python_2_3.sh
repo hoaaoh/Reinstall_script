a9786476553623a4097b7406ab54a4671cdcc32e
hoaaoh <b8i3054755@hotmail.com>
2016-12-01 10:58:11 +0800

    update python script

--- install0_python_2_3.sh
+++ install0_python_2_3.sh
@@ -6,8 +6,8 @@ tar -xvf $filename
 rm -rf $filename
 
 cd ./Python-2.7.12
-./configure --enable-shared --with-ensurepip=install --prefix=/usr/local/lib/python-2.7.12
-make
+./configure --enable-shared --enable-unicode=ucs4 --with-ensurepip=install --enable-ipv6 --prefix=/usr/local
+LD_RUN_PATH=/usr/local/lib make
 make install
 cd ../
 
