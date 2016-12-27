77d19c3ce12efe570b88da33bebf33a26463b723
hoaaoh <b8i3054755@hotmail.com>
2016-12-27 15:26:07 +0800

    update libs to install

--- install0_libs.sh
+++ install0_libs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libsqlite3-dev
 apt-get install git tmux cmake
-
+apt-get install automake autoconf libtool subversion
+apt-get install libatlas3-base
