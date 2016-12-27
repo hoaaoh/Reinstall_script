e26ebfdc9cfce5b674b2d8995c35338333e2224d
hoaaoh <b8i3054755@hotmail.com>
2016-12-27 15:40:21 +0800

    add install kaldi script

---
+++ install1_kaldi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+dir=$(pwd)
+git clone https://github.com/kaldi-asr/kaldi.git kaldi --origin upstream
+cd kaldi
+
+### tools ###
+cd tools
+./extras/check_dependencies.sh
+make -j 8
+cd ..
+### src ### 
+cd src
+make depend -j 8
+make -j 8
+
